7db79367564f261b70a85a74d772d5e184d64e2a
Oleh Halas <oleggalas8@gmail.com>
2026-03-11 13:09:42 +0200

    Implement func that will copy file in current derectory

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    try:
+        com = command.split()
+        cp_com = com[0]
+        fil = com[1]
+        new_file = com[2]
+    except IndexError:
+        return
+
+    if fil == new_file:
+        return
+    if cp_com != "cp":
+        return
+
+    try:
+        with open(fil, "r") as file_in, open(new_file, "w") as file_out:
+            data = file_in.read()
+            file_out.write(data)
+    except FileNotFoundError:
+        return
